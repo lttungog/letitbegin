20fc1e6e50abfe9d28e7a693d2fdc2819943fd0c
lttungog <124686851+lttungog@users.noreply.github.com>
2023-09-18 09:27:09 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lab1/AddNumbers.c
    #	lab1/HW Math Equation.c
    #	lab1/PerimeterAreaOfCircle.c
    #	lab1/SalaryAge.c
    #	lab1/SquareNumber.c
    #	lab1/simple interest.c

---
+++ lab1/Classwork/class 180923/stars.c
@@ -0,0 +1,24 @@
+//
+// Created by ADMIN on 9/18/2023.
+//
+#include <stdio.h>
+
+void drawRec() {
+
+    printf("************");
+    printf("\n*          *");
+    printf("\n*          *");
+    printf("\n*          *");
+    printf("\n************");
+    printf("\n");
+
+}
+
+int main() {
+
+    drawRec();
+    drawRec();
+
+    return 0;
+
+}
