a9a8777e04210647107778edc2a7f8c2ff3db88b
lttungog <124686851+lttungog@users.noreply.github.com>
2023-09-22 09:42:17 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lab1/AddNumbers.c
    #	lab1/HW Math Equation.c
    #	lab1/PerimeterAreaOfCircle.c
    #	lab1/SalaryAge.c
    #	lab1/SquareNumber.c
    #	lab1/simple interest.c

--- lab1/Classwork/End of Class Test/Reverse Numbers.c
+++ lab1/Classwork/End of Class Test/Reverse Numbers.c
@@ -3,8 +3,36 @@
 //
 #include <stdio.h>
 
-int main() {
+void main() {
 
 
+    int arr[100];
+    int secarr[100];
+    int k = 0;
+
+    printf("\nPlease enter 10 numbers: ");
+    printf("\n=============================");
+
+    for (int i = 0; i < 10; i++) {
+        printf("\nNumber %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int j = 9; j >= 0; j--) {
+        secarr[k] = arr[j];
+        k++;
+    }
+
+    /*printf("\nThe numbers in reversed order is: ");
+
+    for (int t = 0; t < 10; t++) {
+        printf("\nNumber %d is %d", t+1, secarr[t]);
+    } */
+
+    printf("\nReversed:");
+
+    for (int b = 0; b < 10; b++) {
+        printf(" %d", secarr[b]);
+    }
 
 }
